840b385bd1b018284fb5dc0fba13d689927ade18
21hook <2765049749@qq.com>
2018-08-13 12:22:39 +0800

    output data to <Recommend/> using recommend api

--- src/components/recommend/Recommend.vue
+++ src/components/recommend/Recommend.vue
@@ -1,12 +1,37 @@
 <template>
   <div>
-    recommend
+    <div v-for="(item, i) in recommends" :key="i">
+      <a :href="item.linkUrl">
+        <img class="needsclick" @load="loadImage" :src="item.picUrl">
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
+import {getRecommend} from 'api/recommend'
+import {ERR_OK} from 'api/config'
+
 export default {
-  name: 'Recommend'
+  name: 'Recommend',
+  data() {
+    return {
+      recommends: []
+    }
+  },
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    // _helperMethod # private methods in the module
+    _getRecommend() {
+      getRecommend().then(res => { // resolved value in the jsonp promise
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  }
 }
 </script>
 
