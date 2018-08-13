49736743ef772eb6dc100867c380835f63c405b8
21hook <2765049749@qq.com>
2018-08-13 10:55:18 +0800

    add the page skeleton(header component & global css)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <!-- component outlet -->
     <router-view/>
   </div>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'assets/css/index.styl' // global import css
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
