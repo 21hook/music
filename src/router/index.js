b167b0641c5e736db91a8e777445056b013a083c
21hook <2765049749@qq.com>
2018-08-13 11:10:09 +0800

    config the routes for tab component

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Recommend from 'components/recommend/Recommend'
+import Singer from 'components/singer/Singer'
+import Search from 'components/search/Search'
+import Rank from 'components/rank/Rank'
+
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  // route/component direct mapping
   routes: [
     {
       path: '/', // site root path
       name: 'HelloWorld'
+    },
+    // route/component direct mapping
+    {
+      path: '/recommend',
+      component: Recommend
+    },
+    {
+      path: '/Singer',
+      component: Singer
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/rank',
+      component: Rank
     }
   ]
 })
