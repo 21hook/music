add0d00ef674d1159d885e15af2ec88bc7c60a8e
21hook <2765049749@qq.com>
2018-08-16 22:31:31 +0800

    fetch the singer list & transform it

--- src/components/singer/Singer.vue
+++ src/components/singer/Singer.vue
@@ -1,15 +1,87 @@
 <template>
-  <div>
+  <div class="singer">
     singer
   </div>
 </template>
 
 <script>
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import Singer from 'common/singer'
+
+const HOT_SINGER_LEN = 10
+const HOT_NAME = '热门'
+
 export default {
-  name: 'Singer'
+  name: 'Singer',
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    // _privateMethod
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = this._normalizeSinger(res.data.list)
+        }
+      })
+    },
+    _normalizeSinger(list) { // 数据聚合整理［热门，a,b,c,d...］
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid
+        }))
+      })
+      // 为了得到有序列表，我们需要处理 map
+      let ret = []
+      let hot = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+
+  }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
