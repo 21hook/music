6bc9daf34e4607df24d50561f5dd68e07d4b51be
21hook <2765049749@qq.com>
2018-08-17 23:14:50 +0800

    fetch data & output data to <rank/>

--- src/components/rank/Rank.vue
+++ src/components/rank/Rank.vue
@@ -1,15 +1,87 @@
 <template>
-  <div>
-    rank
+  <div class="rank">
+    <ul class="toplist">
+      <li v-for="(item, i) in topList" :key="i" class="item">
+        <div class="icon">
+          <img v-lazy="item.picUrl" width="100" height="100">
+        </div>
+        <ul class="songlist">
+          <li v-for="(song, i) in item.songList" :key="i" class="song">
+            <span>{{i + 1}}</span>
+            <span>{{song.songname}}-{{song.singername}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import {getTopList} from 'api/rank'
+import {ERR_OK} from 'api/config'
+
 export default {
-  name: 'Rank'
+  name: 'Rank',
+  data() {
+    return {
+      topList: []
+    }
+  },
+  created() {
+    this._getTopList()
+  },
+  methods: {
+    _getTopList() {
+      getTopList().then(res => {
+        if (res.code === ERR_OK) {
+          this.topList = res.data.topList
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~assets/css/variable"
+  @import "~assets/css/mixin"
 
+  .rank
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .toplist
+      height: 100%
+      overflow: hidden
+      .item
+        display: flex
+        margin: 0 20px
+        padding-top: 20px
+        height: 100px
+        &:last-child
+          padding-bottom: 20px
+        .icon
+          flex: 0 0 100px
+          width: 100px
+          height: 100px
+        .songlist
+          flex: 1
+          display: flex
+          flex-direction: column
+          justify-content: center
+          padding: 0 20px
+          height: 100px
+          overflow: hidden
+          background: $color-highlight-background
+          color: $color-text-d
+          font-size: $font-size-small
+          .song
+            no-wrap()
+            line-height: 26px
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
